3fed1ae6ffda502d84a110b3339025c0e86ea5c9
brosmar18 <bryangonzalez1896@icloud.com>
2024-01-25 14:24:43 -0600

    city size fixed, starship added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,15 @@ import { Suspense, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import Loader from "../components/Loader";
 import City from '../models/City';
+import Space from "../models/Space";
+import StarShip from "../models/StarShip";
 
 const Home = () => {
   const [isRotating, setIsRotating] = useState(false);
 
   const adjustCityScreenSizes = () => {
     let screenScale = null;
-    let screenPosition = [0, -6.5, -43];
+    let screenPosition = [50, -6.5, -120];
     let rotation = [0.1, 4.7, 0];
 
     if (window.innerWidth < 768) {
@@ -25,21 +27,24 @@ const Home = () => {
   return (
     <section className="w-full h-screen relative">
       <Canvas
-        className="w-full h-screen relative"
+        className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
-          <directionalLight />
-          <ambientLight />
-          <pointLight />
-          <spotLight />
-          <hemisphereLight />
-
+          <directionalLight position={[1, 3, 1]} intensity={4} />
+          <ambientLight intensity={0.9} />
+          <hemisphereLight
+            skyColor="#b1e1ff"
+            groundColor="#000000"
+            intensity={1}
+          />
+          <Space />
           <City 
             position={cityPosition}
             scale={cityScale}
             rotation={cityRotation}
           />
+          <StarShip />
         </Suspense>
       </Canvas>
     </section>
